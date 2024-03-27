0d7bf127edcd257974b1061665c83ed960580601
Gustavo-Almeida-M <gustavoam916@gmail.com>
2024-03-27 08:56:23 -0300

    Atualização aula 27-03-2024

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import ClassA from "./components/Atividades/Atividade02/ClassContext";
+import Pai from "./components/Atividades/Atividade01/Questao01/Pai";
 function App() {
   return (
     <div className="App">
       <h1 style={{color:"blue"}}>Carro</h1>
-      <ClassA />
+      <Pai/>
     </div>
   );
 }
